43c0767a80078b30947c487a330d0208857ea9ca
Maria Rita L. Carvalho <mariaritalimacarvalho2004@gmail.com>
2023-03-27 18:28:17 -0300

    Update 02.kt

--- 100_exercícios_de_algoritmos_criados_pelo_Gustavo_Guanabara/exercícios-resolvidos/02.kt
+++ 100_exercícios_de_algoritmos_criados_pelo_Gustavo_Guanabara/exercícios-resolvidos/02.kt
@@ -1,8 +1,10 @@
-// Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas para ela:
-// Ex:
-// Qual é o seu nome? João da Silva
-// Olá João da Silva, é um prazer te conhecer!
-  
+/*
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas para ela:
+Ex:
+Qual é o seu nome? João da Silva
+Olá João da Silva, é um prazer te conhecer!
+*/
+
 fun main() {
     print("Qual é o seu nome? ")
     val nome = readLine()

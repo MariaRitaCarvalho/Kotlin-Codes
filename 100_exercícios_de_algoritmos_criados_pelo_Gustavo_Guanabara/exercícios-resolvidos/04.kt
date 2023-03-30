31e41767d4307dc9a9d7921a8b3df25c97bb4e58
Maria Rita L. Carvalho <mariaritalimacarvalho2004@gmail.com>
2023-03-30 10:34:01 -0300

    Create 04.kt

---
+++ 100_exercícios_de_algoritmos_criados_pelo_Gustavo_Guanabara/exercícios-resolvidos/04.kt
@@ -0,0 +1,24 @@
+/*
+Desenvolva um algoritmo que leia dois números inteiros e mostre o somatório entre eles.
+Ex:
+Digite um valor: 8
+Digite outro valor: 5
+A soma entre 8 e 5 é igual a 13
+*/
+
+fun main() {
+    print("Digite o primeiro número: ")
+    val num1 = readLine()?.toDoubleOrNull()
+    if (num1 == null) {
+        println("Valor inválido inserido para o primeiro número.")
+        return
+    }
+    print("Digite o segundo número: ")
+    val num2 = readLine()?.toDoubleOrNull()
+    if (num2 == null) {
+        println("Valor inválido inserido para o segundo número.")
+        return
+    }
+    val soma = num1 + num2
+    println("A soma de $num1 e $num2 é $soma")
+}
